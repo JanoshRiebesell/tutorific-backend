b9a1b7519f65ff20ddd7e423a96fc4904e254eb8
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-18 18:33:34 +0200

    feat: add empty tutors, students and connections controllers

---
+++ controllers/connections.controller.js
@@ -0,0 +1,5 @@
+'use-strict';
+
+module.exports.createConnection = async (ctx, next) => {};
+module.exports.updateConnection = async (ctx, next) => {};
+module.exports.deleteConnection = async (ctx, next) => {};
---
+++ controllers/tutors.controller.js
@@ -0,0 +1,7 @@
+'use-strict';
+
+module.exports.getTutor = async (ctx, next) => {};
+module.exports.getTutors = async (ctx, next) => {};
+module.exports.createTutor = async (ctx, next) => {};
+module.exports.updateTutor = async (ctx, next) => {};
+module.exports.deleteTutor = async (ctx, next) => {};

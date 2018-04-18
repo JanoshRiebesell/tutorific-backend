6bfe6784582bbe5ca629ea11cacbeb42ea041510
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-18 23:11:40 +0200

    refactor: add person model object for use in tutors and students model

---
+++ models/persons.model.js
@@ -0,0 +1,47 @@
+'use  strict';
+
+module.exports = {
+  firstname: {
+    type: String,
+    required: 'A person must provide a firstname!',
+    trim: true,
+    minlength: 3,
+    maxlength: 50
+  },
+  lastname: {
+    type: String,
+    required: 'A person must provide a lastname!',
+    trim: true,
+    minlength: 3,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: 'A person must provide an email!',
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Invalid email!']
+  },
+  phone: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    validate: [(num) => {validator.isMobilePhone(num, 'any')}, 'Invalid phone number!']
+  },
+  address: {
+    type: String,
+    required: 'A person must provide an address or city district.'
+  },
+  status: {
+    type: String,
+    enum: ['registered', 'active', 'inactive', 'deleted'],
+    default: 'registered'
+  },
+  birthday: Date,
+  birthplace: String,
+  gender: {
+    type: String,
+    enum: ['male', 'female', 'other'],
+  }
+}
--- models/tutors.model.js
+++ models/tutors.model.js
@@ -1,63 +1,22 @@
+'use strict';
+
 const mongoose = require('mongoose');
 const validator = require('validator');
+const person = require('./persons.model');
 const Subject = require('./subjects.model');
 
-const Schema = mongoose.Schema;
-
-const TutorSchema = new Schema({
-  firstname: {
-    type: String,
-    required: 'A tutor must provide a firstname!',
-    trim: true,
-    minlength: 3,
-    maxlength: 50
-  },
-  lastname: {
-    type: String,
-    required: 'A tutor must provide a lastname!',
-    trim: true,
-    minlength: 3,
-    maxlength: 50
-  },
-  email: {
-    type: String,
-    required: 'A tutor must provide an email!',
-    unique: true,
-    lowercase: true,
-    trim: true,
-    validate: [validator.isEmail, 'Invalid email!']
-  },
-  phone: {
-    type: String,
-    lowercase: true,
-    trim: true,
-    validate: [(num) => {validator.isMobilePhone(num, 'any')}, 'Invalid phone number!']
-  },
+const TutorSchema = new mongoose.Schema({
+  ...person,
   subjects: {
     type: [mongoose.Schema.ObjectId],
     ref: 'Subject'
-  },
-  address: {
-    type: String,
-    required: 'A tutor must provide a public space or city district where the tutoring should take place.'
-  },
-  status: {
-    type: String,
-    enum: ['registered', 'active', 'inactive', 'deleted'],
-    default: 'registered'
   },
   fieldOfStudy: String,
   semester: {
     type: Number,
     min: 1,
     max: 50
-  },
-  birthday: Date,
-  birthplace: String,
-  gender: {
-    type: String,
-    enum: ['male', 'female', 'other'],
   }
-}, {timestamps: true})
+}, {timestamps: true});
 
 module.exports = mongoose.model('Tutor', TutorSchema);

51e18166c737769cf275d24261970c21ebc34b37
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-18 18:23:44 +0200

    chore: add boilerplate koa app

---
+++ index.js
@@ -0,0 +1,29 @@
+'use-strict';
+
+const Koa = require('koa');
+const logger = require('koa-logger');
+const compress = require('koa-compress');
+const bodyParser = require('koa-bodyparser');
+const cors = require('kcors');
+require('dotenv').config()
+
+// const router = require('./router');
+
+const app = new Koa();
+
+app.use(logger());
+app.use(cors());
+app.use(bodyParser());
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use(compress());
+
+if (!module.parent) {
+  const ip = process.env.ip || 'localhost';
+  const port = process.env.port || 3000;
+  app.listen(port, ip, () => {
+    console.log(`Server running at http://${ip}:${port}`);
+  });
+}

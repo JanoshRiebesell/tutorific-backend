752288069ba7b90a12ef676462e344a214a4737b
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-18 22:42:20 +0200

    feat: add subjects model

---
+++ models/subjects.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const SubjectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    enum: ['Math', 'Physics', 'Chemistry', 'Biology', 'English',
+      'Spanish', 'French', 'German', 'History', 'Politics',
+      'Philosophy', 'Economics', 'Physical Education'],
+    required: 'Subject must have a name!'
+  },
+  grade: {
+    type: Number,
+    min: 1,
+    max: 13
+  }
+});
+
+module.exports = mongoose.model('Subject', SubjectSchema);

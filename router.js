68f4a6792de9f49e6daba5eaec00615eb33c4686
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-19 11:19:37 +0200

    fix: typo in router for controller imports + add url param id

--- router.js
+++ router.js
@@ -1,27 +1,27 @@
 'use strict';
 
 const Router = require('koa-router');
-const tutorController = require('./controllers/tutor.controller');
-const studentController = require('./controllers/student.controller');
-const connectionController = require('./controllers/connection.controller');
+const tutorController = require('./controllers/tutors.controller');
+const studentController = require('./controllers/students.controller');
+const connectionController = require('./controllers/connections.controller');
 
 const router = new Router;
 
-router.get('/tutor', tutorController.getTutor);
+router.get('/tutor/:id', tutorController.getTutor);
 router.get('/tutors', tutorController.getTutors);
-router.get('/student', studentController.getStudent);
+router.get('/student/:id', studentController.getStudent);
 router.get('/students', studentController.getStudents);
 
 router.post('/tutor', tutorController.createTutor);
 router.post('/student', studentController.createStudent);
 router.post('/connection', connectionController.createConnection);
 
-router.put('/tutor', tutorController.updateTutor);
-router.put('/student', studentController.updateStudent);
-router.put('/connection', connectionController.updateConnection);
+router.put('/tutor/:id', tutorController.updateTutor);
+router.put('/student/:id', studentController.updateStudent);
+router.put('/connection/:id', connectionController.updateConnection);
 
-router.delete('/tutor', tutorController.deleteTutor);
-router.delete('/student', studentController.deleteStudent);
-router.delete('/connection', connectionController.deleteConnection);
+router.delete('/tutor/:id', tutorController.deleteTutor);
+router.delete('/student/:id', studentController.deleteStudent);
+router.delete('/connection/:id', connectionController.deleteConnection);
 
 module.exports = router;

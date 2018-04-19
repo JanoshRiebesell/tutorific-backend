aacc2f8de99038f5e4a910235ec960d1f74b7146
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-19 15:33:49 +0200

    feat: add connections controller

--- controllers/connections.controller.js
+++ controllers/connections.controller.js
@@ -1,5 +1,15 @@
 'use strict';
 
-module.exports.createConnection = async (ctx, next) => {};
-module.exports.updateConnection = async (ctx, next) => {};
-module.exports.deleteConnection = async (ctx, next) => {};
+const Connection = require('../models/connections.model');
+
+module.exports.createConnection = async (ctx, next) => {
+  ctx.body = await Connection.create(ctx.request.body);
+};
+
+module.exports.updateConnection = async (ctx, next) => {
+  ctx.body = await Connection.findByIdAndUpdate(ctx.params.id, ctx.request.body, {new: true});
+};
+
+module.exports.deleteConnection = async (ctx, next) => {
+  ctx.body = await Connection.findByIdAndRemove(ctx.params.id);
+};

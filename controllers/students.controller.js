45b0c853d1153d40a4c2f140c67aeaeb2dd09acd
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-19 11:21:01 +0200

    feat: implement tutors and students controller

--- controllers/students.controller.js
+++ controllers/students.controller.js
@@ -1,7 +1,23 @@
 'use strict';
 
-module.exports.getStudent = async (ctx, next) => {};
-module.exports.getStudents = async (ctx, next) => {};
-module.exports.createStudent = async (ctx, next) => {};
-module.exports.updateStudent = async (ctx, next) => {};
-module.exports.deleteStudent = async (ctx, next) => {};
+const Student = require('../models/students.model');
+
+module.exports.getStudent = async (ctx, next) => {
+  ctx.body = await Student.findById(ctx.params.id);
+};
+
+module.exports.getStudents = async (ctx, next) => {
+  ctx.body = await Student.find();
+};
+
+module.exports.createStudent = async (ctx, next) => {
+  ctx.body = await Student.create(ctx.request.body);
+};
+
+module.exports.updateStudent = async (ctx, next) => {
+  ctx.body = await Student.findByIdAndUpdate(ctx.params.id, ctx.request.body, {new: true});
+};
+
+module.exports.deleteStudent = async (ctx, next) => {
+  ctx.body = await Student.findByIdAndRemove(ctx.params.id);
+};
--- controllers/tutors.controller.js
+++ controllers/tutors.controller.js
@@ -1,7 +1,23 @@
 'use strict';
 
-module.exports.getTutor = async (ctx, next) => {};
-module.exports.getTutors = async (ctx, next) => {};
-module.exports.createTutor = async (ctx, next) => {};
-module.exports.updateTutor = async (ctx, next) => {};
-module.exports.deleteTutor = async (ctx, next) => {};
+const Tutor = require('../models/tutors.model');
+
+module.exports.getTutor = async (ctx, next) => {
+  ctx.body = await Tutor.findById(ctx.params.id);
+};
+
+module.exports.getTutors = async (ctx, next) => {
+  ctx.body = await Tutor.find();
+};
+
+module.exports.createTutor = async (ctx, next) => {
+  ctx.body = await Tutor.create(ctx.request.body);
+};
+
+module.exports.updateTutor = async (ctx, next) => {
+  ctx.body = await Tutor.findByIdAndUpdate(ctx.params.id, ctx.request.body, {new: true});
+};
+
+module.exports.deleteTutor = async (ctx, next) => {
+  ctx.body = await Tutor.findByIdAndRemove(ctx.params.id);
+};

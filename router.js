0068939f536e9b7828d41d56256da6426b0ea431
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-27 22:17:51 +0200

    feat: add get connection(s) routes and update Postman collection

--- router.js
+++ router.js
@@ -1,27 +1,34 @@
 'use strict';
 
 const Router = require('koa-router');
+
 const tutorsController = require('./controllers/tutors.controller');
 const studentsController = require('./controllers/students.controller');
 const connectionsController = require('./controllers/connections.controller');
+const authorizationController = require('./controllers/authorization.controller');
+
+const authMiddleware = require('./middlewares/authorization').authorize;
 
 const router = new Router;
 
-router.get('/tutor/:id', tutorsController.getTutor);
-router.get('/tutors', tutorsController.getTutors);
 router.get('/student/:id', studentsController.getStudent);
 router.get('/students', studentsController.getStudents);
-
-router.post('/tutor', tutorsController.createTutor);
 router.post('/student', studentsController.createStudent);
-router.post('/connection', connectionsController.createConnection);
+router.put('/student/:id', authMiddleware, studentsController.updateStudent);
+router.delete('/student/:id', authMiddleware, studentsController.deleteStudent);
 
-router.put('/tutor/:id', tutorsController.updateTutor);
-router.put('/student/:id', studentsController.updateStudent);
-router.put('/connection/:id', connectionsController.updateConnection);
+router.get('/tutor/:id', tutorsController.getTutor);
+router.get('/tutors', tutorsController.getTutors);
+router.post('/tutor', tutorsController.createTutor);
+router.put('/tutor/:id', authMiddleware, tutorsController.updateTutor);
+router.delete('/tutor/:id', authMiddleware, tutorsController.deleteTutor);
 
-router.delete('/tutor/:id', tutorsController.deleteTutor);
-router.delete('/student/:id', studentsController.deleteStudent);
-router.delete('/connection/:id', connectionsController.deleteConnection);
+router.get('/connection/:id', connectionsController.getconnection);
+router.get('/connections', connectionsController.getconnections);
+router.post('/connection', authMiddleware, connectionsController.createConnection);
+router.put('/connection/:id', authMiddleware, connectionsController.updateConnection);
+router.delete('/connection/:id', authMiddleware, connectionsController.deleteConnection);
+
+router.get('/login', authorizationController.login);
 
 module.exports = router;

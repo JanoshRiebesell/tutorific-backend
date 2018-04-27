feab079c36982160c4e61a129a3edd0f3484f1c9
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-27 22:15:20 +0200

    feat: improve http status code setting of centralized error handler

--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -2,7 +2,8 @@ module.exports = async (ctx, next) => {
   try {
     await next();
   } catch (err) {
-    ctx.body = err.message;
-    ctx.status = 500;
+    ctx.body = err.stack;
+    ctx.status = err.status || 500;
+    if (err.name === 'ValidationError' || err.name === 'BulkWriteError') ctx.status = 400;
   }
 };

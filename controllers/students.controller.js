77dcb282271c0e6667ac406cc9fb06610ee128b1
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-27 22:18:45 +0200

    feat: add granular error handling on controllers

--- controllers/students.controller.js
+++ controllers/students.controller.js
@@ -4,10 +4,12 @@ const Student = require('../models/student.model');
 
 module.exports.getStudent = async (ctx, next) => {
   ctx.body = await Student.findById(ctx.params.id);
+  ctx.assert(ctx.body, 404, `Could not find student with id ${ctx.params.id}.`);
 };
 
 module.exports.getStudents = async (ctx, next) => {
   ctx.body = await Student.find();
+  ctx.assert(ctx.body, 404, `Could not find any students.`);
 };
 
 module.exports.createStudent = async (ctx, next) => {
@@ -15,7 +17,10 @@ module.exports.createStudent = async (ctx, next) => {
 };
 
 module.exports.updateStudent = async (ctx, next) => {
-  ctx.body = await Student.findByIdAndUpdate(ctx.params.id, ctx.request.body, {new: true});
+  ctx.body = await Student.findByIdAndUpdate(
+    ctx.params.id, ctx.request.body,
+    {new: true, runValidators: true}
+  );
 };
 
 module.exports.deleteStudent = async (ctx, next) => {

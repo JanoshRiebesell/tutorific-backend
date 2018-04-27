f306224d896fbf772732d3c518e9439f72641b95
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-27 22:18:45 +0200

    feat: add granular error handling on controllers

--- controllers/connections.controller.js
+++ controllers/connections.controller.js
@@ -2,12 +2,25 @@
 
 const Connection = require('../models/connection.model');
 
+module.exports.getConnection = async (ctx, next) => {
+  ctx.body = await Connection.findById(ctx.params.id);
+  ctx.assert(ctx.body, 404, `Could not find connection with id ${ctx.params.id}.`);
+};
+
+module.exports.getConnections = async (ctx, next) => {
+  ctx.body = await Connection.find();
+  ctx.assert(ctx.body, 404, `Could not find any connections.`);
+};
+
 module.exports.createConnection = async (ctx, next) => {
   ctx.body = await Connection.create(ctx.request.body);
 };
 
 module.exports.updateConnection = async (ctx, next) => {
-  ctx.body = await Connection.findByIdAndUpdate(ctx.params.id, ctx.request.body, {new: true});
+  ctx.body = await Connection.findByIdAndUpdate(
+    ctx.params.id, ctx.request.body,
+    {new: true, runValidators: true}
+  );
 };
 
 module.exports.deleteConnection = async (ctx, next) => {
--- controllers/tutors.controller.js
+++ controllers/tutors.controller.js
@@ -4,10 +4,12 @@ const Tutor = require('../models/tutor.model');
 
 module.exports.getTutor = async (ctx, next) => {
   ctx.body = await Tutor.findById(ctx.params.id);
+  ctx.assert(ctx.body, 404, `Could not find tutor with id ${ctx.params.id}.`);
 };
 
 module.exports.getTutors = async (ctx, next) => {
   ctx.body = await Tutor.find();
+  ctx.assert(ctx.body, 404, `Could not find any tutors.`);
 };
 
 module.exports.createTutor = async (ctx, next) => {
@@ -15,7 +17,10 @@ module.exports.createTutor = async (ctx, next) => {
 };
 
 module.exports.updateTutor = async (ctx, next) => {
-  ctx.body = await Tutor.findByIdAndUpdate(ctx.params.id, ctx.request.body, {new: true});
+  ctx.body = await Tutor.findByIdAndUpdate(
+    ctx.params.id, ctx.request.body,
+    {new: true, runValidators: true}
+  );
 };
 
 module.exports.deleteTutor = async (ctx, next) => {

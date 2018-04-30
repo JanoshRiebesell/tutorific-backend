593b6fbfb10b21ff3e119ff8162b7a04872da806
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-30 20:46:27 +0200

    refactor: controllers comply with CRUD sequence

--- controllers/students.controller.js
+++ controllers/students.controller.js
@@ -4,6 +4,14 @@ const bcrypt = require('bcrypt');
 
 const Student = require('../models/student.model');
 
+module.exports.createStudent = async (ctx, next) => {
+  const {password} = ctx.request.body;
+  ctx.assert(password, 400, 'Cannot create new student without password!');
+  ctx.request.body.passwordHash = await bcrypt.hash(password, 1);
+  const newStudent = await Student.create(ctx.request.body);
+  ctx.body = `Created new student with id ${newStudent._id}.`;
+};
+
 module.exports.getStudent = async (ctx, next) => {
   ctx.body = await Student.findById(ctx.params.id);
   ctx.assert(ctx.body, 404, `Could not find student with id ${ctx.params.id}.`);
@@ -12,14 +20,6 @@ module.exports.getStudent = async (ctx, next) => {
 module.exports.getStudents = async (ctx, next) => {
   ctx.body = await Student.find();
   ctx.assert(ctx.body, 404, `Could not find any students.`);
-};
-
-module.exports.createStudent = async (ctx, next) => {
-  const {password} = ctx.request.body;
-  ctx.assert(password, 400, 'Cannot create new student without password!');
-  ctx.request.body.passwordHash = await bcrypt.hash(password, 1);
-  const newStudent = await Student.create(ctx.request.body);
-  ctx.body = `Created new student with id ${newStudent._id}.`;
 };
 
 module.exports.updateStudent = async (ctx, next) => {
--- controllers/tutors.controller.js
+++ controllers/tutors.controller.js
@@ -4,6 +4,14 @@ const bcrypt = require('bcrypt');
 
 const Tutor = require('../models/tutor.model');
 
+module.exports.createTutor = async (ctx, next) => {
+  const {password} = ctx.request.body;
+  ctx.assert(password, 400, 'Cannot create new tutor without password!');
+  ctx.request.body.passwordHash = await bcrypt.hash(password, 1);
+  const newTutor = await Tutor.create(ctx.request.body);
+  ctx.body = `Created new tutor with id ${newTutor._id}.`;
+};
+
 module.exports.getTutor = async (ctx, next) => {
   ctx.body = await Tutor.findById(ctx.params.id);
   ctx.assert(ctx.body, 404, `Could not find tutor with id ${ctx.params.id}.`);
@@ -12,14 +20,6 @@ module.exports.getTutor = async (ctx, next) => {
 module.exports.getTutors = async (ctx, next) => {
   ctx.body = await Tutor.find();
   ctx.assert(ctx.body, 404, `Could not find any tutors.`);
-};
-
-module.exports.createTutor = async (ctx, next) => {
-  const {password} = ctx.request.body;
-  ctx.assert(password, 400, 'Cannot create new tutor without password!');
-  ctx.request.body.passwordHash = await bcrypt.hash(password, 1);
-  const newTutor = await Tutor.create(ctx.request.body);
-  ctx.body = `Created new tutor with id ${newTutor._id}.`;
 };
 
 module.exports.updateTutor = async (ctx, next) => {

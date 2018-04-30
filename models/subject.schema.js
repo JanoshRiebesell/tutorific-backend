23ad10e63910c5466c578faeb3c192d258b72967
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-30 10:16:37 +0200

    feat: subject schema complies with simplified subjects string array

--- models/subject.schema.js
+++ models/subject.schema.js
@@ -1,15 +1,9 @@
 'use strict';
 
-const mongoose = require('mongoose');
-
-module.exports = new mongoose.Schema({
-  name: {
-    type: String,
-    enum: ['Math', 'Physics', 'Chemistry', 'Biology', 'Computer Science', 'English',
-      'Spanish', 'French', 'German', 'Italian', 'Chinese', 'Japanese', 'Latin', 'History', 'Politics', 'Ethics',
-      'Social Studies', 'Philosophy', 'Economics', 'Physical Education', 'Music', 'Art'],
-    required: 'Subject must have a name!'
-  },
-}, {
-  _id: false
-});
+module.exports = {
+  type: String,
+  enum: ['Math', 'Physics', 'Chemistry', 'Biology', 'Computer Science', 'English',
+  'Spanish', 'French', 'German', 'Italian', 'Chinese', 'Japanese', 'Latin', 'History', 'Politics', 'Ethics',
+  'Social Studies', 'Philosophy', 'Economics', 'Physical Education', 'Music', 'Art'],
+  required: 'A subject must have a name!',
+};

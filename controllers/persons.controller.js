7821a659d8d3ef0d331e17bc1b08027cbaba9b73
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-22 23:15:55 +0200

    feat: remove single-person get controller and extend multi-person get controller to handle it

--- controllers/persons.controller.js
+++ controllers/persons.controller.js
@@ -16,40 +16,42 @@ module.exports.createPerson = async (ctx, next) => {
   ctx.assert(ctx.body, 400, `Could not create ${userType} from provided request body.`);
 };
 
-module.exports.getPerson = async (ctx, next) => {
-  ctx.assert(ctx.params.id, 400, 'An id must be provided in order to retrieve a person!');
-  ctx.body = await Student.findById(ctx.params.id);
-  if (!ctx.body) ctx.body = await Tutor.findById(ctx.params.id);
-  ctx.assert(ctx.body, 404, `Could not find a tutor or student with id ${ctx.params.id}.`);
-};
-
-module.exports.getPersons = async (ctx, next) => {
-  const {userType, lat, lng, status, lastLoginAfter, maxDistance} = ctx.request.query;
-  ctx.assert(userType === 'student' || userType === 'tutor', 400, 'Please provide a user type of either student or tutor!');
-  const Person = userType === 'student' ? Student : Tutor;
-  const dbQuery = {};
-  if (status) dbQuery.status = status;
+const constructDbQuery = ({lat, lng, lastLoginAfter, maxDistance, ...rest}) => {
+  const dbQuery = {...rest};
   if (lastLoginAfter) dbQuery.updatedAt = {$gte: new Date(lastLoginAfter)};
   if (lat && lng) dbQuery.location = {
     $near: {
       $geometry: {type: 'Point', coordinates: [lng, lat]},
-      $maxDistance: maxDistance * 1000
+      $maxDistance: maxDistance * 1000,
     }
   };
-  ctx.body = await Person.find(dbQuery);
-  ctx.assert(ctx.body, 404, `Could not find any ${userType}s based on these filters.`);
+  return dbQuery;
+};
+
+module.exports.getPersons = async (ctx, next) => {
+  const {userType} = ctx.request.query;
+  const dbQuery = constructDbQuery(ctx.request.query);
+  if (userType === 'student') ctx.body = await Student.find(dbQuery);
+  else if (userType === 'tutor') ctx.body = await Tutor.find(dbQuery);
+  else {
+    const students = await Student.find(dbQuery);
+    const tutors = await Tutor.find(dbQuery);
+    ctx.body = [...students, ...tutors];
+  }
+  ctx.status = 200;
+  ctx.assert(ctx.body, 204, `Could not find any ${userType || 'user'}s based on these filters.`);
 };
 
 module.exports.updatePerson = async (ctx, next) => {
   ctx.assert(ctx.params.id, 400, 'An id must be provided in order to update a person!');
   ctx.assert(ctx.request.body, 400, 'An request body must be provided in order to update a person!');
-  ctx.body = await Person.findByIdAndUpdate(ctx.params.id, ctx.request.body, {new: true, runValidators: true});
+  ctx.body = await Person.findOneAndUpdate(ctx.params.id, ctx.request.body, {new: true, runValidators: true});
   if (!ctx.body) ctx.body = await Person.findByIdAndUpdate(ctx.params.id, ctx.request.body, {new: true, runValidators: true});
-  ctx.assert(ctx.body, 404, `Could not find a tutor or student with id ${ctx.params.id}.`);
+  ctx.assert(ctx.body, 204, `Could not find a tutor or student with id ${ctx.params.id}.`);
 };
 
 module.exports.deletePerson = async (ctx, next) => {
   ctx.assert(ctx.params.id, 400, 'An id must be provided in order to delete a person!');
   ctx.body = await Person.findByIdAndRemove(ctx.params.id);
-  ctx.assert(ctx.body, 404, `Could not find a tutor or student with id ${ctx.params.id}.`);
+  ctx.assert(ctx.body, 204, `Could not find a tutor or student with id ${ctx.params.id}.`);
 };

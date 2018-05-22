b5dc6661d89d299c7106023d6903b519e4f03543
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-22 23:13:44 +0200

    feat: add production and development config to db.js

--- db.js
+++ db.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhost/tutorific');
-
-mongoose.connection.on('connected', () => {
-  console.log('Connected to local Tutorific database');
-});
+if (process.env.NODE_ENV === 'development') {
+  mongoose.connect('mongodb://localhost/tutorific');
+  mongoose.connection.on('connected', () => {
+    console.log('Connected to local Tutorific database');
+  });
+} else if (process.env.NODE_ENV === 'production') {
+  mongoose.connect(
+    `mongodb://
+      ${process.env.DB_USER}:
+      ${process.env.DB_PASSWORD}
+      @ds229790.mlab.com:29790/tutorific`
+  );
+  mongoose.connection.on('connected', () => {
+    console.log('Connected to mLab Tutorific database');
+  });
+}

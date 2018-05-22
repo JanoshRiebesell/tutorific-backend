8078de01b725354d98e26979d921575336254afb
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-22 23:13:44 +0200

    feat: add production and development config to db.js

--- models/subject.schema.js
+++ models/subject.schema.js
@@ -2,8 +2,9 @@
 
 module.exports = {
   type: String,
-  enum: ['Math', 'Physics', 'Chemistry', 'Biology', 'Computer Science', 'English',
-  'Spanish', 'French', 'German', 'Italian', 'Chinese', 'Japanese', 'Latin', 'History', 'Politics', 'Ethics',
-  'Social Studies', 'Philosophy', 'Economics', 'Physical Education', 'Music', 'Art'],
+  enum: ['Math', 'Physics', 'Chemistry', 'Biology', 'Computer Science',
+    'English', 'Spanish', 'French', 'German', 'Italian', 'Chinese',
+    'Japanese', 'Latin', 'History', 'Politics', 'Ethics', 'Social Studies',
+    'Philosophy', 'Economics', 'Physical Education', 'Music', 'Art'],
   required: 'A subject must have a name!',
 };

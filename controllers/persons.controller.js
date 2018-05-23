177701a980ce90eff980f07aed6f20f3c143342f
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-23 21:31:08 +0200

    feat: improve query command and no-content handling in personController

--- controllers/persons.controller.js
+++ controllers/persons.controller.js
@@ -29,29 +29,42 @@ const constructDbQuery = ({lat, lng, lastLoginAfter, maxDistance, ...rest}) => {
 };
 
 module.exports.getPersons = async (ctx, next) => {
-  const {userType} = ctx.request.query;
+  const {userType, command} = ctx.request.query;
+  if (command) delete ctx.request.query.command;
   const dbQuery = constructDbQuery(ctx.request.query);
-  if (userType === 'student') ctx.body = await Student.find(dbQuery);
-  else if (userType === 'tutor') ctx.body = await Tutor.find(dbQuery);
+  const cmd = command || 'find';
+
+  if (userType === 'student') ctx.body = await Student[cmd](dbQuery);
+  else if (userType === 'tutor') ctx.body = await Tutor[cmd](dbQuery);
   else {
-    const students = await Student.find(dbQuery);
-    const tutors = await Tutor.find(dbQuery);
-    ctx.body = [...students, ...tutors];
+    const students = await Student[cmd](dbQuery);
+    const tutors = await Tutor[cmd](dbQuery);
+    if (cmd === 'count') ctx.body = students + tutors;
+    else ctx.body = [...students, ...tutors];
   }
-  ctx.status = 200;
-  ctx.assert(ctx.body, 204, `Could not find any ${userType || 'user'}s based on these filters.`);
+  if (!ctx.body) {
+    ctx.status = 204;
+    ctx.message = `Could not find any ${userType || 'user'}s based on these filters.`;
+  }
 };
 
 module.exports.updatePerson = async (ctx, next) => {
   ctx.assert(ctx.params.id, 400, 'An id must be provided in order to update a person!');
   ctx.assert(ctx.request.body, 400, 'An request body must be provided in order to update a person!');
-  ctx.body = await Person.findOneAndUpdate(ctx.params.id, ctx.request.body, {new: true, runValidators: true});
-  if (!ctx.body) ctx.body = await Person.findByIdAndUpdate(ctx.params.id, ctx.request.body, {new: true, runValidators: true});
-  ctx.assert(ctx.body, 204, `Could not find a tutor or student with id ${ctx.params.id}.`);
+  ctx.body = await Student.findOneAndUpdate(ctx.params.id, ctx.request.body, {new: true, runValidators: true});
+  if (!ctx.body) ctx.body = await Tutor.findByIdAndUpdate(ctx.params.id, ctx.request.body, {new: true, runValidators: true});
+  if (!ctx.body) {
+    ctx.status = 204;
+    ctx.message = `Could not find a tutor or student with id ${ctx.params.id}.`;
+  }
 };
 
 module.exports.deletePerson = async (ctx, next) => {
   ctx.assert(ctx.params.id, 400, 'An id must be provided in order to delete a person!');
-  ctx.body = await Person.findByIdAndRemove(ctx.params.id);
-  ctx.assert(ctx.body, 204, `Could not find a tutor or student with id ${ctx.params.id}.`);
+  ctx.body = await Student.findByIdAndRemove(ctx.params.id);
+  if (!ctx.body) ctx.body = await Tutor.findByIdAndRemove(ctx.params.id);
+  if (!ctx.body) {
+    ctx.status = 204;
+    ctx.message = `Could not find a tutor or student with id ${ctx.params.id}.`;
+  }
 };
